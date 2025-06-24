4845b6bb44d6dbe176c624b67e8f03821fe63bdf
Matheus Augusto Paschuinio <matheus.paschuinio@fatec.sp.gov.br>
2025-06-24 10:24:11 -0300

    Map

---
+++ funcoes/callback_3.js
@@ -0,0 +1,9 @@
+
+const nums = [1, 2, 3, 4, 5]
+const dobro = n => n * 2
+console.log(nums.map(dobro))
+
+const nomes = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa']
+const primeiraLetra = texto => texto[0]
+const letras = nomes.map(primeiraLetra)
+console.log(nomes, letras)
